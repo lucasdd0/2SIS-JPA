be0822375b96f4717d8180645442ee7111b31750
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-13 20:42:21 -0300

    commit

--- src/main/java/br/fiap/assistencia_tecnica/domain/Cliente.java
+++ src/main/java/br/fiap/assistencia_tecnica/domain/Cliente.java
@@ -9,4 +9,44 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID_CLIENTE")
     private Long id;
+
+    @Column(name = "NOME", nullable = true, length = 120)
+    private String nome;
+
+    @Column(name = "EMAIL", nullable = false, length = 120)
+    private String email;
+
+    @Column(name = "TELEFONE", nullable = false, length = 120)
+    private String telefone;
+
+    @Column(name = "SENHA", nullable = false, length = 120)
+    private String senha;
+
+    public Cliente(Long id, String nome, String email, String telefone, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
 }
