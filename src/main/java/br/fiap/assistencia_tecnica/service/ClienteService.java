1a1c86c13228b4e3ec32636f9b330d14f70700cb
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-15 22:37:39 -0300

    commit

--- src/main/java/br/fiap/assistencia_tecnica/service/ClienteService.java
+++ src/main/java/br/fiap/assistencia_tecnica/service/ClienteService.java
@@ -1,5 +1,24 @@
 package br.fiap.assistencia_tecnica.service;
 
-public class ClienteService {
+import br.fiap.assistencia_tecnica.domain.Cliente;
+import br.fiap.assistencia_tecnica.repository.ClienteRepository;
+import br.fiap.assistencia_tecnica.web.dto.ClienteDTO;
+import org.springframework.stereotype.Service;
 
+@Service
+public class ClienteService {
+    private ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository){
+        this.clienteRepository = clienteRepository;
+    }
+
+    public Cliente salvar(ClienteDTO clienteDTO){
+        Cliente cliente = new Cliente();
+        cliente.setNome(clienteDTO.getNome());
+        cliente.setEmail(clienteDTO.getEmail());
+        cliente.setTelefone(clienteDTO.getTelefone());
+        cliente.setSenha(clienteDTO.getSenha());
+        return clienteRepository.save(cliente);
+    }
 }
--- src/main/java/br/fiap/assistencia_tecnica/web/controller/ClienteController.java
+++ src/main/java/br/fiap/assistencia_tecnica/web/controller/ClienteController.java
@@ -2,11 +2,9 @@ package br.fiap.assistencia_tecnica.web.controller;
 
 import br.fiap.assistencia_tecnica.domain.Cliente;
 import br.fiap.assistencia_tecnica.service.ClienteService;
+import br.fiap.assistencia_tecnica.web.dto.ClienteDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cliente")
@@ -20,7 +18,7 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar(Cliente cliente){
-        return cliente;
+    public Cliente salvar(@RequestBody ClienteDTO clienteDTO){
+        return service.salvar(clienteDTO);
     }
 }
