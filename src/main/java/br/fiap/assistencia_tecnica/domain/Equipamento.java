8dd74238e0f2b661ed36e803b5af02fa1fa92051
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-22 22:09:54 -0300

    commit

---
+++ src/main/java/br/fiap/assistencia_tecnica/domain/Equipamento.java
@@ -0,0 +1,18 @@
+package br.fiap.assistencia_tecnica.domain;
+
+import java.time.LocalDate;
+
+public class Equipamento {
+    private Long id;
+    private Cliente cliente;
+    private String tipo;
+    private String marca;
+    private String modelo;
+    private String numeroSerie;
+    private LocalDate dataCadastro;
+
+
+
+
+
+}
